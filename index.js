7b664ad11c7918d6a8cec1348862c335e3ed3243
Ettienne Pitts <ettpitts@gmail.com>
2018-03-20 10:37:56 +0200

    Get vacation days added

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-exports.test = function() {
-  console.log('This is a test')
-}
+/**
+ * Get all the vacation days of a certain country
+ * @param {string} country
+ */
+const getVacationDays = country => {
+  return require(`./src/vacation-days/src/${country}/vacation-days.json`);
+};
+
+module.exports = {
+  getVacationDays
+};
