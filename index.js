746c5774b3ecb3fbb75fd9ca632309e185bd6d68
Ettienne Pitts <ettpitts@gmail.com>
2018-03-20 12:31:08 +0200

    Vacation day functions added

--- index.js
+++ index.js
@@ -1,11 +1,70 @@
 /**
+ * @typedef {Object} VacationDays
+ * @property {{[name: string]: string}} constants Vacation days that are always on the same day
+ * @property {{[year: string]: {[name: string]: string}}} variable Vacation days that change from year to year
+ */
+
+/**
  * Get all the vacation days of a certain country
  * @param {string} country
+ * @returns {VacationDays}
  */
 const getVacationDays = country => {
   return require(`./src/vacation-days/src/${country}/vacation-days.json`);
 };
 
+/**
+ * Get all the vacation days for the year in which the date is
+ * @param {string} country
+ * @param {Date} date
+ * @returns {{[name: string]: string}}
+ */
+const getVacationDaysForYear = (country, date) => {
+  const vacationDays = getVacationDays(country);
+
+  return Object.assign(
+    vacationDays.constants,
+    vacationDays.variable[date.getFullYear().toString()]
+  );
+};
+
+/**
+ * Determine if the specified date is a vacation day in the specified country
+ *
+ * @param {string} country
+ * @param {Date} date
+ * @returns {bool}
+ */
+const isVacationDay = (country, date) => {
+  const vacationDays = getVacationDaysForYear(country, date);
+
+  const vacationDayNames = Object.keys(vacationDays);
+
+  for (let count = 0; count < vacationDayNames.length; count++) {
+    if (compareStringAndDate(vacationDays[vacationDayNames[count]], date)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+/**
+ * @param {string} monthDay
+ * @param {Date} date
+ * @returns {bool}
+ */
+const compareStringAndDate = (monthDay, date) => {
+  return (
+    ('0' + (date.getMonth() + 1)).slice(-2) +
+      '-' +
+      ('0' + date.getDate()).slice(-2) ===
+    monthDay
+  );
+};
+
 module.exports = {
-  getVacationDays
+  getVacationDays,
+  getVacationDaysForYear,
+  isVacationDay
 };
